9c19d035876a4a97a5c677263772ce52fd1c427e
PoojaVeera <110709890+PoojaVeera@users.noreply.github.com>
2022-08-25 12:20:44 +0530

    modified:   src/App.js 	deleted:    src/components/Todo.js 	deleted:    src/components/TodoList.js 	deleted:    src/components/Todoform.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Child } from "./components/Child";
-import TodoList from "./components/TodoList.js";
-import data from "./data.json";
 function App() {
-  const [toDo, setToDo] = useState(data);
   return (
     <div>
-      <center>
-        <Child />
-        <TodoList todoList={todoList} />
-      </center>
+      <Child />
     </div>
   );
 }
--- src/components/Todo.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Todo = ({ todo }) => {
-  return <div>{todo.task}</div>;
-};
